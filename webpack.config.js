255f0b8c57249a333263901317e4a000d3277bd9
Jorjeee3 <look7at7george@gmail.com>
2023-02-22 11:39:57 -0500

    fix bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js', './src/normalize.css', './src/index.css', './src/services/weather/months.js', './src/services/weather/Weather.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'production',
+//   optimization: {
+//     // [...]
+//     minimize: true,
+//   },
   module: {
     rules: [
       {
@@ -22,4 +29,12 @@ module.exports = {
       },
     ],
   },
+//   optimization: {
+//     minimizer: [
+//       // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+//       // `...`,
+//       new CssMinimizerPlugin(),
+//     ],
+//   },
+//   plugins: [new MiniCssExtractPlugin()],
 };
