19708abf4516bd93ac6771e98b65b509cddd41b5
Jorjeee3 <look7at7george@gmail.com>
2023-02-22 12:05:27 -0500

    minor

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export async function getCities (searchText) {
     const options = {
         method: 'GET',
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: ['./src/index.js', './src/normalize.css', './src/index.css', './src/months.js', './src/weather.js'],
@@ -9,10 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
-//   optimization: {
-//     // [...]
-//     minimize: true,
-//   },
   module: {
     rules: [
       {
@@ -29,12 +23,4 @@ module.exports = {
       },
     ],
   },
-//   optimization: {
-//     minimizer: [
-//       // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-//       // `...`,
-//       new CssMinimizerPlugin(),
-//     ],
-//   },
-//   plugins: [new MiniCssExtractPlugin()],
 };
